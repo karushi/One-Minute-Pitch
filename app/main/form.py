8f2dcc572c632867aa609b2df28ba66bbde521e0
karushi <karuship@gmail.com>
2018-02-13 15:30:41 +0300

    import pitchForm

--- app/main/form.py
+++ app/main/form.py
@@ -12,20 +12,13 @@ class LineForm(FlaskForm):
     '''
    Class to create a wtf form for creating a pitch
     '''
-   line_content = StringField('One Minute Pitch', validators=[Required()])
-   submit = SubmitField('Submit')
+    line_content = StringField('One Minute Pitch', validators=[Required()])
+    submit = SubmitField('Submit')
 
 
 class CommentForm(FlaskForm):
     '''
    Class to create a wtf form for creating a feedback on a pitch
     '''
-   comment_content =  TextAreaField('Comment', validators=[Required()])
-   submit = SubmitField('Submit')
-
-
-class UpvoteForm(FlaskForm):
-    '''
-   Class to create a wtf form for upvoting a pitch
-    '''
-   submit = SubmitField('Upvote')
+    comment_content = TextAreaField('Comment', validators=[Required()])
+    submit = SubmitField('Submit')
