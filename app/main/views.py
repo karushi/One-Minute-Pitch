1a61cb4b961ae4e937dff230808d6e00f415df0d
karushi <karuship@gmail.com>
2018-02-13 15:31:21 +0300

    added class pitch

--- app/main/views.py
+++ app/main/views.py
@@ -2,9 +2,29 @@ from . import main
 from flask import render_template, request, redirect, url_for, abort
 from ..models import User, Pitch
 from flask_login import login_required
+from .form import PitchForm
 
 
 @main.route('/')
 def index():
     pitches = 'hello world'
     return render_template('index.html', pitches=pitches)
+
+
+@main.route('/pitch/new', methods=['GET', 'POST'])
+@login_required
+def new_pitch():
+    '''
+    View new pitch route function that returns a page with a form to create a category
+    '''
+    form = PitchForm()
+
+    if form.validate_on_submit():
+        name = form.name.data
+        new_pitch = Pitch(name=name)
+        new_pitch.save_pitches()
+
+        return redirect(url_for('.index'))
+
+    title = 'New Pitch'
+    return render_template('new_pitch.html', pitch_form = form)
